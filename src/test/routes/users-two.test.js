d7ce27fe1891acc6e94d0423be81dd4735254ccc
chaptone <rakkan.j@gmail.com>
2021-01-17 22:20:52 +0700

    Add parallel test context

--- src/test/routes/users-two.test.js
+++ src/test/routes/users-two.test.js
@@ -1,59 +1,16 @@
 const request = require("supertest");
 const buildApp = require("../../app");
-const pool = require("../../pool");
 const UserRepo = require("../../repos/user-repo");
+const Context = require("../context");
 
-const { randomBytes } = require("crypto");
-const { default: migrate } = require("node-pg-migrate");
-const format = require("pg-format");
+let context;
 
 beforeAll(async () => {
-  const roleName = "a" + randomBytes(4).toString("hex");
-
-  await pool.connect({
-    host: "localhost",
-    port: 5432,
-    database: "socialnetwork-test",
-    user: "prisma",
-    password: "prisma",
-  });
-
-  await pool.query(
-    format("CREATE ROLE %I WITH LOGIN PASSWORD %L;", roleName, roleName),
-  );
-
-  await pool.query(
-    format("CREATE SCHEMA %I AUTHORIZATION %I;", roleName, roleName),
-  );
-
-  await pool.close();
-
-  await migrate({
-    schema: roleName,
-    direction: "up",
-    log: () => {},
-    noLock: true,
-    dir: "migrations",
-    databaseUrl: {
-      host: "localhost",
-      port: 5432,
-      database: "socialnetwork-test",
-      user: roleName,
-      password: roleName,
-    },
-  });
-
-  await pool.connect({
-    host: "localhost",
-    port: 5432,
-    database: "socialnetwork-test",
-    user: roleName,
-    password: roleName,
-  });
+  context = await Context.build();
 });
 
 afterAll(() => {
-  return pool.close();
+  return context.close();
 });
 
 it("create a user", async () => {
